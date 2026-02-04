963aa5f5c6786e708afaafbd9bde3c3aa15857d3
Yugant <ynagrala@iu.edu>
2026-02-04 17:46:21 -0500

    2 exercises done

--- apputil.py
+++ apputil.py
@@ -2,7 +2,6 @@ import seaborn as sns
 import pandas as pd
 
 
-# update/add code below ...
 def fibonacci(n):
     if n<=1:
         return n
@@ -18,6 +17,14 @@ print(fibonacci(9))
 def to_binary(n):
     if n<=1:
         return n
-    
     while n>1:
-        
+        x= n%2 + 10*to_binary(n//2)
+        return x         
+print(to_binary(18))
+
+
+url = 'https://github.com/melaniewalsh/Intro-Cultural-Analytics/raw/master/book/data/bellevue_almshouse_modified.csv'
+
+df_bellevue = pd.read_csv(url)
+
+df_bellevue
