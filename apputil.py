495313cbd2312966fefb1e201094f60efa0e4eca
Hamza Almani <github-halmani@iu.edu>
2026-02-04 17:20:11 -0500

    initial-commit

--- apputil.py
+++ apputil.py
@@ -3,3 +3,21 @@ import pandas as pd
 
 
 # update/add code below ...
+def fibonacci(n):
+    if n<=1:
+        return n
+    
+    while n>1:
+        x= fibonacci(n-1) + fibonacci(n-2)
+        return x
+
+print(fibonacci(9))
+
+
+
+def to_binary(n):
+    if n<=1:
+        return n
+    
+    while n>1:
+        
